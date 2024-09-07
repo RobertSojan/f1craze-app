f96db852f9e672d171504e4849c4b931daeb8bc8
Robert Sojan <robertsojan2609@gmail.com>
2024-09-07 08:44:50 -0500

    Enable SSL postgres connection

--- db/db.js
+++ db/db.js
@@ -4,7 +4,12 @@ require('dotenv').config;
 
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL // Use environment variable
+  connectionString: process.env.DATABASE_URL, // Use environment variable
+
+  ssl: {
+    rejectUnauthorized: false
+  }
+
 });
 
 module.exports = pool;
